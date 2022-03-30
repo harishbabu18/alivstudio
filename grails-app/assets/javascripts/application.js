f70427a27a3ce15287940faccafacc1a9a6768f3
B K HARISH BABU <babuamuda@gmail.com>
2022-03-30 09:19:40 +0530

    first commit

---
+++ grails-app/assets/javascripts/application.js
@@ -0,0 +1,15 @@
+// This is a manifest file that'll be compiled into application.js.
+//
+// Any JavaScript file within this directory can be referenced here using a relative path.
+//
+// You're free to add application-wide JavaScript to this file, but it's generally better
+// to create separate JavaScript files as needed.
+//
+//= require imagesloaded.pkgd.min
+//= require masonry.pkgd.min
+//= require BigPicture.min
+//= require purecounter.min
+//= require aos.min
+//= require bootstrap.bundle.min
+//= require main
+//= require_self
---
+++ grails-app/init/alivstudio/BootStrap.groovy
@@ -0,0 +1,9 @@
+package alivstudio
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
